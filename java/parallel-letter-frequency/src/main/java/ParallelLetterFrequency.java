dec7bf9d843d98c6ef2ffb3d1c1cc85488a78584
vivek.mittal <vivek.mittal@redbus.com>
2024-03-30 08:34:07 +0530

    add first commit

---
+++ java/parallel-letter-frequency/src/main/java/ParallelLetterFrequency.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+class ParallelLetterFrequency {
+
+    ParallelLetterFrequency(String[] texts) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Map<Character, Integer> countLetters() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
---
+++ python/leap/leap.py
@@ -0,0 +1,11 @@
+def leap_year(year):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            return True
+        else:
+            return False
+
+    if (year % 4 == 0):
+        return True
+
+    return False
